fa1f042470e1655bd0237ce8cc4da7e551e464bd
Jalen Solis <jaysol@192.168.0.243>
2020-10-04 16:29:58 -0700

    Creates an Adding Machine that will use methods, to work on the total variable

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,8 +1,10 @@
 package cse360assignment02;
+//Jalen Solis
 
 public class AddingMachine 
 {
   private int total;
+  private StringBuffer formula = new StringBuffer("0");
   
   public AddingMachine ()
   {
@@ -11,26 +13,37 @@ public class AddingMachine
   
   public int getTotal ()
   {
-    return 0;
+    return total;
   }
   
-  public void add (int value)
+  public void add (int value)// add value to total
   {
-	  
+	 total+= value;
+	 formula.append(" + " + value);
   }
 
-  public void subtract (int value) 
+  public void subtract (int value) // subtract value from total
   {
-	  
+	  total = total - value;
+	  formula.append(" - " + value);
   }
 
-  public String toString ()
+  public String toString ()// returns the operation
   {
-    return "";
+    return formula.toString();
   }
 
-  public void clear()
+  public void clear()// clears memory
   {
-	  
+	  System.gc();
   }
+  public static void main(String[] args)  //Test run
+  { 
+  	AddingMachine test = new AddingMachine();
+  	test.add(4);
+  	test.subtract(2);
+  	test.add(5); 
+  	System.out.println(test.toString());
+  }
+
 }
